a8ad5558b4b72510921ec49cc233e3cb1f30ac87
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2018-03-12 11:58:19 -0400

    Initial commit

---
+++ rpi-ws281x-native/rpi-ws281x-native.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = function(RED) {
+    function Ws281xNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+
+        node.leds = require("rpi-ws281x-native");
+        node.numLeds = parseInt(config.numleds);
+        node.mode = config.mode;
+        node.rowLength = config.rowLength;
+
+        node.finalArray = new Uint32Array(node.numLeds);
+        node.leds.init(node.numLeds, {dmaNum: 10});
+
+        node.on('input', function(msg) {
+            const mode = msg.mode || node.mode;
+
+            if (Buffer.isBuffer(msg.payload)) {
+                node.bufferToArray(msg.payload, node.numLeds, mode);
+                node.leds.render(node.leds);
+            }
+        });
+
+        node.on('close', function() {
+            try {
+                node.leds.reset();
+            } catch (e) {
+                node.error("Error closing LEDs");
+            }
+        });
+
+        node.bufferToArray = (buffer, numLeds, mode) => {
+            if (mode == 'canvas') {
+                const rowLength = 8;
+                for (let p = 0; p < numLeds; p++) {
+                    let rowNum = Math.floor(p/rowLength);
+                    // Si impair
+                    if (rowNum % 2 == 1) {
+                        node.finalArray[(2 * rowNum * rowLength) - p + rowLength - 1] = buffer.readUInt32LE(p*4);
+                    } else {
+                        node.finalArray[p] = buffer.readUInt32LE(p*4);
+                    }
+                }
+            } else {
+                // For each LED
+                for (var p = 0; p < numLeds; p++) {
+                    var pixel = Buffer.alloc(4);
+                    buffer.copy(pixel, 1, p*3, (p*3)+3);
+                    node.finalArray[p] = pixel.readUInt32BE(0);
+                }
+            }
+
+        };
+    }
+    RED.nodes.registerType("rpi-ws281x-native", Ws281xNode);
+};
