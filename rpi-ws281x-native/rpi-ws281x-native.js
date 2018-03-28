4a5df222035e44f80a445344ecf4653a6d9f5bea
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2018-03-28 14:52:40 -0400

    Remove mode and add support for 4 channels

--- rpi-ws281x-native/rpi-ws281x-native.js
+++ rpi-ws281x-native/rpi-ws281x-native.js
@@ -11,9 +11,14 @@ module.exports = function(RED) {
         node.height = parseInt(config.height);
 
         node.numLeds = node.width * node.height;
-        node.mode = config.mode;
         node.reverse = config.reverse;
         node.stripType = config.striptype;
+        if (node.stripType.includes('w')) {
+            node.numChannels = 4;
+        } else {
+            node.numChannels = 3;
+        }
+
 
         node.brightness = Math.floor(parseInt(config.brightness) * 255 / 100);
 
@@ -26,10 +31,8 @@ module.exports = function(RED) {
         node.finalArray = node.leds.array;
 
         node.on('input', function(msg) {
-            const mode = msg.mode || node.mode;
-
             if (Buffer.isBuffer(msg.payload)) {
-                node.bufferToArray(msg.payload, node.numLeds, mode, node.width);
+                node.bufferToArray(msg.payload, node.numLeds, node.width);
                 ws281x.render(node.finalArray);
             }
         });
@@ -42,26 +45,16 @@ module.exports = function(RED) {
             }
         });
 
-        node.bufferToArray = (buffer, numLeds, mode, rowLength) => {
-            if (mode == 'canvas') {
-                for (let p = 0; p < numLeds; p++) {
-                    let rowNum = Math.floor(p/rowLength);
-                    // Si impair
-                    if ((rowNum % 2 == 1) && node.reverse) {
-                        node.finalArray[(2 * rowNum * rowLength) - p + rowLength - 1] = buffer.readUInt32LE(p*4);
-                    } else {
-                        node.finalArray[p] = buffer.readUInt32LE(p*4);
-                    }
-                }
-            } else {
-                // For each LED
-                for (var p = 0; p < numLeds; p++) {
-                    var pixel = Buffer.alloc(4);
-                    buffer.copy(pixel, 1, p*3, (p*3)+3);
-                    node.finalArray[p] = pixel.readUInt32BE(0);
+        node.bufferToArray = (buffer, numLeds, rowLength) => {
+            for (let p = 0; p < numLeds; p++) {
+                let rowNum = Math.floor(p/rowLength);
+                // Si impair
+                if ((rowNum % 2 == 1) && node.reverse) {
+                    node.finalArray[(2 * rowNum * rowLength) - p + rowLength - 1] = buffer.readUInt32LE(p * node.numChannels);
+                } else {
+                    node.finalArray[p] = buffer.readUInt32LE(p * node.numChannels);
                 }
             }
-
         };
     }
     RED.nodes.registerType("rpi-ws281x-native", Ws281xNode);
