95408c212763d82e6653e588f5b31fa3eedaa0f0
Nathanaël Lécaudé <natcl@users.noreply.github.com>
2018-03-29 10:48:13 -0400

    Towards something universal

--- rpi-ws281x-native/rpi-ws281x-native.js
+++ rpi-ws281x-native/rpi-ws281x-native.js
@@ -11,8 +11,14 @@ module.exports = function(RED) {
         node.height = parseInt(config.height);
 
         node.numLeds = node.width * node.height;
+        node.mode = config.mode;
         node.reverse = config.reverse;
         node.stripType = config.striptype;
+        if (node.stripType.match(/-...w/g)) {
+            node.numChannels = 4;
+        } else {
+            node.numChannels = 3;
+        }
 
         node.brightness = Math.floor(parseInt(config.brightness) * 255 / 100);
 
@@ -25,28 +31,51 @@ module.exports = function(RED) {
         node.finalArray = node.leds.array;
 
         node.on('input', function(msg) {
+            const mode = msg.mode || node.mode;
+
             if (Buffer.isBuffer(msg.payload)) {
-                node.bufferToArray(msg.payload, node.numLeds, node.width);
+                node.bufferToArray(msg.payload, node.numLeds, mode, node.width);
                 ws281x.render(node.finalArray);
             }
         });
 
         node.on('close', function() {
             try {
-                ws281x.finalize();
+                //ws281x.finalize();
             } catch (e) {
                 node.error("Error closing LEDs");
             }
         });
 
-        node.bufferToArray = (buffer, numLeds, rowLength) => {
+        node.bufferToArray = (buffer, numLeds, mode, rowLength) => {
             for (let p = 0; p < numLeds; p++) {
                 let rowNum = Math.floor(p/rowLength);
-                // Si impair
+                // Odd line
                 if ((rowNum % 2 == 1) && node.reverse) {
-                    node.finalArray[(2 * rowNum * rowLength) - p + rowLength - 1] = buffer.readUInt32LE(p * 4);
+                    if (mode == 'canvas') {
+                        node.finalArray[(2 * rowNum * rowLength) - p + rowLength - 1] = buffer.readUInt32LE(p*4);
+                    } else {
+                        let pixel = Buffer.alloc(4);
+                        if (node.numChannels == 3) {
+                            buffer.copy(pixel, 1, p*3, (p*3)+3);
+                        } else {
+                            buffer.copy(pixel, 0, p*4, (p*4)+4);
+                        }
+                        node.finalArray[(2 * rowNum * rowLength) - p + rowLength - 1] = pixel.readUInt32BE(0);
+                    }
+                // Even line
                 } else {
-                    node.finalArray[p] = buffer.readUInt32LE(p * 4);
+                    if (mode == 'canvas') {
+                        node.finalArray[p] = buffer.readUInt32LE(p*4);
+                    } else {
+                        let pixel = Buffer.alloc(4);
+                        if (node.numChannels == 3) {
+                            buffer.copy(pixel, 1, p*3, (p*3)+3);
+                        } else {
+                            buffer.copy(pixel, 0, p*4, (p*4)+4);
+                        }
+                        node.finalArray[p] = pixel.readUInt32BE(0);
+                    }
                 }
             }
         };
